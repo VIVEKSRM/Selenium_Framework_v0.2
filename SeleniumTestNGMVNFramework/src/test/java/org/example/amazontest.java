55acb455d586894332c6024a63373627ba325f84
VIVEKSRM <vivek.ranjan.srm@gmail.com>
2024-05-23 22:48:45 +0530

    Basic framework Structure

---
+++ SeleniumTestNGMVNFramework/src/test/java/org/example/amazontest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class amazontest {
+    @Test()
+    public void setup()
+    {
+        WebDriver driver = null;
+        WebDriverManager.chromedriver().browserVersion("77.0.3865.40").setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("enable-automation");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+    }
+
+}
